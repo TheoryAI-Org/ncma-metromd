6ae9feb5e71295560aa28401643c25ea4546534a
Mike Endale <mike@blencorp.com>
2025-02-12 14:45:53 -0600

    Updated the meta data and fixed the navbar on mobile.

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -37,7 +37,7 @@ export function NavBar() {
         {/* Mobile Navigation */}
         <Sheet>
           <SheetTrigger asChild className="md:hidden">
-            <Button variant="ghost" size="icon" className="text-white">
+            <Button variant="ghost" size="icon" className="bg-[#1B365D] text-white hover:bg-[#2A4A7F]">
               <Menu className="h-6 w-6" />
               <span className="sr-only">Open menu</span>
             </Button>
